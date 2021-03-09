4f211e76424b64925e6bfee9770764eb05a93d7f
Adam Etzion <adametzion90@gmail.com>
2021-03-09 14:42:45 -0700

    Add user registratyion sad path testing, udpate model, cntrollers as needed to fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: false }, presence: true
-  validates_presence_of :password, require: true
-  validates_presence_of :secure_password, require: true
-  # validates_presence_of :api_key, uniqueness: true, require: true
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
+  validates :password, confirmation: { case_sensitive: true }
+
   has_secure_password
+  has_secure_token :api_key
 
   before_save { email.downcase! }
-  before_create :set_api_key
-
-  def set_api_key
-    self.api_key = generate_api_key
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,29 @@ require 'rails_helper'
 
 describe User, type: :model do
   describe "validations" do
-    it {should validate_presence_of(:email)}
-    it {should validate_presence_of(:password)}
-    it {should validate_presence_of(:api_key)}
-    it {should validate_uniqueness_of(:api_key)}
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :password}
+    it {should validate_presence_of :password_confirmation}
+  end
+
+  describe "registration happy paths" do
+    it "ensures email is case insensitive" do
+      email = 'EXAMPLE@eXaMpLe.CoM'
+      user = User.create!(email: email, password: 'password', password_confirmation: 'password')
+
+      expect(user.email).to eq('example@example.com')
+    end
+  end
+
+  describe "registration sad paths" do
+    it "email is blank" do
+      user = User.new(email: '', password: 'password', password_confirmation: 'password')
+      expect(user.save).to eq(false)
+    end
+    
+    it "password doesn't match password confirmation" do
+      user = User.new(email: "example@example.com", password: 'password', password_confirmation: 'paSsword')
+      expect(user.save).to eq(false)
+    end
   end
 end
