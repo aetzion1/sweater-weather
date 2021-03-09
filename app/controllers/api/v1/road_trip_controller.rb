b0e5bd819110ff0573876b4b760092b2160366c7
Adam Etzion <adametzion90@gmail.com>
2021-03-09 16:26:11 -0700

    Add controller for road trip create method

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     return render_invalid_parameters('No parameters required, please') if request.query_parameters.present?
-    roadtrip = RoadTrip.new(input)
-    if roadtrip.save
-      render json: RoadTripSerializer.new(user), status: :created
+    # wouldnt it be better to just ignore the params, rather than make invalid? ^ ^ ^
+    return render_invalid_input('Incorrect api_key'), status: :unauthorized unless User.find_by(api_key: roadtrip_params[:api_key]) 
+
+    if roadtrip_params[:origin].present? & roadtrip_params[:destination].present?
+      roadtrip = RoadTripFacade.get_roadtrip(roadtrip_params[:origin], roadtrip_params[:destination])
     else
-      render_invalid_input
+      render_invalid_input('Please specify locations')
     end
+    render json: (roadtrip ? RoadTripSerializer.new(roadtrip), status: :created : { data: {} })
+  end
+  
+  private
+
+  def roadtrip_params
+    params.permit(:origin, :destination, :api_key)
   end
 end
