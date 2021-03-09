29c01dff6fb3fed888e68f75c941600e62b5f1ca
Adam Etzion <adametzion90@gmail.com>
2021-03-09 16:37:18 -0700

    Add tarvel service for road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -12,8 +12,6 @@ class RoadTripFacade
       weather_at_eta = RoadTripForecast.new(response[:current])
 
       arrival_time = Time.at(((Time.now + seconds).to_r / (30*60)).round * (30*60))
-      # travel_data = TravelService.get_travel(food, coordinates, arrival_time)
-      # Travel = Travel.new(travel_data)
 
       RoadTrip.new(start_city, end_city, travel_time, weather_at_eta)
     end
---
+++ app/services/travel_service.rb
@@ -0,0 +1,19 @@
+class RestaurantService
+  class << self
+    def travel_time(origin, destination)
+      response = map_quest_conn.get('route') do |req|
+        req.params[:from] = origin
+        req.params[:to] = destination
+      end
+      data = JSON.parse(response.body, symbolize_names: true)
+      data[:route][:realTime]
+    end
+
+    def map_quest_conn
+      Faraday.new(
+        url: 'http://www.mapquestapi.com/directions/v2/',
+        params: { key: ENV['MAPQUEST_GEOCODING_API_KEY'] }
+      )
+    end
+  end
+end
