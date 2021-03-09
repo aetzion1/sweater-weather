2fddfa56cc51a753e839b0dd8fc96ad39fb8154f
Adam Etzion <adametzion90@gmail.com>
2021-03-09 16:05:31 -0700

    Update road trip spec, minor fixes to login request spec

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,38 +1,42 @@
 require 'rails_helper'
 
-describe "login API" do
+describe "road trip API" do
   before :each do
     @user = User.create!(email: "whatever@example.com", password: 'password', password_confirmation: 'password')
   end
 
-	describe "login happy paths" do
+	describe "road trip happy paths" do
 		it "logs a user in based on specific input", :vcr do
 			headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
       body = {
-        "email": "whatever@example.com",
-        "password": "password"
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "#{@user.api_key}"
       }
-      post '/api/v1/sessions', params: body.to_json, headers: headers
+      post '/api/v1/road_trip', params: body.to_json, headers: headers
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
 
       session = JSON.parse(response.body, symbolize_names: true)
-      expect(session[:data][:id]).to eq(@user.id.to_s)
-      expect(session[:data][:type]).to eq('users')
-      expect(session[:data][:attributes][:email]).to eq(@user.email)
-      expect(session[:data][:attributes][:api_key]).to eq(@user.api_key)
+      expect(session[:data][:id]).to eq(nil)
+      expect(session[:data][:type]).to eq('roadtrip')
+      expect(session[:data][:attributes][:start_city]).to eq("Denver, CO")
+      expect(session[:data][:attributes][:end_city]).to eq("Estes Park, CO")
+      expect(session[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Numeric)
+      expect(session[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
     end
   end
 
-  describe "login sad paths" do
+  describe "road trip sad paths" do
     it 'returns an error if header ACCEPT is missing' do 
       headers = {'CONTENT_TYPE' => 'application/json'}
-      body = {
-        "email": "whatever@example.com",
-        "password": "password"
+      body =   {
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "#{@user.api_key}"
       }
-      post '/api/v1/sessions', params: body.to_json, headers: headers
+      post '/api/v1/road_trip', params: body.to_json, headers: headers
 
       expect(response.status).to eq(400)
       errors = JSON.parse(response.body, symbolize_names: true)
@@ -46,10 +50,11 @@ describe "login API" do
     it 'returns an error if header CONTENT_TYPE is missing' do 
       headers = {'ACCEPT' => 'application/json'}
       body = {
-        "email": "whatever@example.com",
-        "password": "password"
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "#{@user.api_key}"
       }
-      post '/api/v1/sessions', params: body.to_json, headers: headers
+      post '/api/v1/road_trip', params: body.to_json, headers: headers
 
 
       expect(response.status).to eq(400)
@@ -63,7 +68,7 @@ describe "login API" do
 
     it 'returns an error if body is missing' do
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
-      post '/api/v1/sessions', headers: headers
+      post '/api/v1/road_trip', headers: headers
   
       expect(response.status).to eq(400)
       errors = JSON.parse(response.body, symbolize_names: true)
@@ -74,13 +79,14 @@ describe "login API" do
       expect(errors[:errors][0]).to be_a(String)
     end
   
-    it 'returns an error if email is blank' do
+    it 'returns an error if origin is blank' do
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
       body = {
-        "email": "",
-        "password": "password"
+        "origin": "",
+        "destination": "Pueblo,CO",
+        "api_key": "#{@user.api_key}"
       }
-      post '/api/v1/sessions', params: body.to_json, headers: headers
+      post '/api/v1/road_trip', params: body.to_json, headers: headers
 
   
       expect(response.status).to eq(400)
@@ -92,13 +98,14 @@ describe "login API" do
       expect(errors[:errors][0]).to be_a(String)
     end
     
-    it 'returns an error if password is blank' do
+    it 'returns an error if destination is blank' do
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
       body = {
-        "email": "whatever@example.com",
-        "password": "",
+        "origin": "Denver,CO",
+        "destination": "",
+        "api_key": "#{@user.api_key}"
       }
-      post '/api/v1/sessions', params: body.to_json, headers: headers
+      post '/api/v1/road_trip', params: body.to_json, headers: headers
 
   
       expect(response.status).to eq(400)
@@ -110,13 +117,14 @@ describe "login API" do
       expect(errors[:errors][0]).to be_a(String)
     end
     
-    it 'returns an error if password is incorrect' do
+    it 'returns an error if api_key is blank' do
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
-      body = {
-        "email": "whatever@example.com",
-        "password": "paSsword"
+      body =   {
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": ""
       }
-      post '/api/v1/sessions', params: body.to_json, headers: headers
+      post '/api/v1/road_trip', params: body.to_json, headers: headers
 
   
       expect(response.status).to eq(400)
@@ -128,13 +136,14 @@ describe "login API" do
       expect(errors[:errors][0]).to be_a(String)
     end
     
-    it 'returns an error if user doesnt exist' do
+    it 'returns an error if api_key is incorrect' do
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
       body = {
-        "email": "whateverdude@example.com",
-        "password": "password"
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "wrongkey"
       }
-      post '/api/v1/sessions', params: body.to_json, headers: headers
+      post '/api/v1/road_trip', params: body.to_json, headers: headers
 
   
       expect(response.status).to eq(400)
@@ -145,14 +154,15 @@ describe "login API" do
       expect(errors[:errors]).to be_an(Array)
       expect(errors[:errors][0]).to be_a(String)
     end
-
+    
     it 'returns an error if parameters are passed' do
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
       body = {
-        "email": "whatever@example.com",
-        "password": "password"
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "wrongkey"
       }
-      post '/api/v1/sessions?email=maliciousturtle@example.com', params: body.to_json, headers: headers
+      post '/api/v1/road_trip?email=maliciousturtle@example.com', params: body.to_json, headers: headers
   
       expect(response.status).to eq(400)
       errors = JSON.parse(response.body, symbolize_names: true)
