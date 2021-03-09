9b56f7de4c2192b2d5e245998fe95ad3fbc9a0d6
Adam Etzion <adametzion90@gmail.com>
2021-03-09 11:46:06 -0700

    Add user spec and user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+  validates_presence_of :api_key, uniqueness: true, require: true
 end
