72b5a16d945258f0e456096529ff2ab87099b7f3
Adam Etzion <adametzion90@gmail.com>
2021-03-09 09:18:41 -0700

    Update background route and controller

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,16 +1,19 @@
 class Api::V1::WeatherController < ApplicationController
   def forecast
     return render_invalid_parameters if !(params[:location].present?)
-    forecast = WeatherFacade.get_forecast(forecast_params[:location])
+    forecast = WeatherFacade.get_forecast(weather_params[:location])
     render json: ForecastSerializer.new(forecast)
   end
 
   def background
+    return render_invalid_parameters if !(params[:location].present?)
+    background = WeatherFacade.get_image(weather_params[:location])
+    render json: BackgroundSerializer.new(background)
   end
 
   private
 
-  def forecast_params
+  def weather_params
     params.permit(:location)
   end
 end
