d082e7b6e8e5101a04c085b9c992c99a66f9cb2f
Adam Etzion <adametzion90@gmail.com>
2021-03-09 11:48:12 -0700

    Add bcrypt gem and has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
   validates_presence_of :api_key, uniqueness: true, require: true
+
+  has_secure_password
 end
