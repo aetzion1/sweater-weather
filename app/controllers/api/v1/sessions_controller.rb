8884b4ca4954fa838b671a53b590698ed39e3d34
Adam Etzion <adametzion90@gmail.com>
2021-03-10 11:29:23 -0700

    hide api keys

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     return render_invalid_parameters('No parameters required, please') if request.query_parameters.present?
-
-    user = User.find_by(email: session_params[:email])
+# DOWNCASE THE EMAIL!!!
+    user = User.find_by(email: session_params[:email].downcase)
     if user&.authenticate(session_params[:password])
       render json: UsersSerializer.new(user), status: :ok
     else
